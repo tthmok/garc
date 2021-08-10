2df0497b9d185044fec280cad6cba0b5956831d0
Terrance Mok <tthmok@gmail.com>
2021-08-10 14:06:32 -0600

    Basic dice roll bot

--- garc.js
+++ garc.js
@@ -1,14 +1,56 @@
-const http = require('http');
+const dotenv = require('dotenv');
+dotenv.config();
 
-const hostname = '127.0.0.1';
-const port = 3000;
+console.log('BOT_USERNAME: ' + process.env.BOT_USERNAME);
+console.log('CHANNEL_NAME: ' + process.env.CHANNEL_NAME);
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello Worlddd');
-});
+const tmi = require('tmi.js');
 
-server.listen(port, hostname, () => {
-  console.log(`Server running at http://${hostname}:${port}/`);
-});
+// Define configuration options
+const opts = {
+  identity: {
+    username: process.env.BOT_USERNAME,
+    password: process.env.OAUTH_TOKEN
+  },
+  channels: [
+    process.env.CHANNEL_NAME
+  ]
+};
+
+// Create a client with our options
+const client = new tmi.client(opts);
+
+// Register our event handlers (defined below)
+client.on('message', onMessageHandler);
+client.on('connected', onConnectedHandler);
+
+// Connect to Twitch:
+client.connect();
+
+// Called every time a message comes in
+function onMessageHandler (target, context, msg, self) {
+  if (self) { return; } // Ignore messages from the bot
+
+  // Remove whitespace from chat message
+  const commandName = msg.trim();
+
+  // If the command is known, let's execute it
+  if (commandName === '!dice') {
+    const num = rollDice();
+    client.say(target, `You rolled a ${num}`);
+    console.log(`* Executed ${commandName} command`);
+  } else {
+    console.log(`* Unknown command ${commandName}`);
+  }
+}
+
+// Function called when the "dice" command is issued
+function rollDice () {
+  const sides = 6;
+  return Math.floor(Math.random() * sides) + 1;
+}
+
+// Called every time the bot connects to Twitch chat
+function onConnectedHandler (addr, port) {
+  console.log(`* Connected to ${addr}:${port}`);
+}
