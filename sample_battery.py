d08c8a6058583801c22bf889e96eb9c990eb678c
Terrance Mok <tthmok@gmail.com>
2021-08-24 12:47:20 -0600

    Fix warnings
    Remove await for is_connected.
    Remove loop parameter for async.sleep call

--- sample_battery.py
+++ sample_battery.py
@@ -3,6 +3,9 @@ import platform
 
 from bleak import BleakClient
 
+CUBE_ID_PURPLE = "F7:7C:30:71:FC:BA"
+CUBE_ID_YELLOW = "E1:F2:90:2E:8A:91"
+
 #Battery, LED and motor characteristics
 BATTERY_CHARACTERISTIC_UUID = ("10b20108-5b3b-4571-9508-cf3efcd7bbae")
 LAMP_CHARACTERISTIC_UUID = ("10b20103-5b3b-4571-9508-cf3efcd7bbae")
@@ -11,7 +14,7 @@ MOTOR_CHARACTERISTIC_UUID = ("10b20102-5b3b-4571-9508-cf3efcd7bbae")
 
 async def run(address, loop):
     async with BleakClient(address, loop=loop) as client:
-        x = await client.is_connected()
+        x = client.is_connected
         #logger.info("Connected: {0}".format(x))
         print("Connected: {0}".format(x))
         #Battery level reading
@@ -23,13 +26,23 @@ async def run(address, loop):
         #Motor: 100 speeds in front of the left, 20 speeds in the back of the right
         write_value = bytearray(b'\x01\x01\x01\x64\x02\x02\x14')
         await client.write_gatt_char(MOTOR_CHARACTERISTIC_UUID, write_value)
-        #Ends after 5 seconds
-        await asyncio.sleep(5.0, loop=loop)
+        #Ends after 3 seconds
+        await asyncio.sleep(3.0)
 
 if __name__ == "__main__":
     address = (
         # discovery.Set the device address of the toio Core Cube found by py here
-        "E1:F2:90:2E:8A:91"  #For Windows or Linux, specify a hexadecimal 6-byte device address.
+        CUBE_ID_PURPLE  #For Windows or Linux, specify a hexadecimal 6-byte device address.
+        if platform.system() != "Darwin"
+        else "243E23AE-4A99-406C-B317-18F1BD7B4CBE"  #UUID attached to macOS for macOS
+    )
+    
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(run(address, loop))
+
+    address = (
+        # discovery.Set the device address of the toio Core Cube found by py here
+        CUBE_ID_YELLOW  #For Windows or Linux, specify a hexadecimal 6-byte device address.
         if platform.system() != "Darwin"
         else "243E23AE-4A99-406C-B317-18F1BD7B4CBE"  #UUID attached to macOS for macOS
     )
