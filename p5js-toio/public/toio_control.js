eb60043b7c8e4720f116b5d6aade98b12f4c7871
Terrance Mok <tthmok@gmail.com>
2021-10-01 15:06:46 -0600

    Fix timings (had to divide everything by 10)
    The command duration is actually sent in a range 1-255 and that gets multiplied by 10 to get how many milliseconds to run things for

--- p5js-toio/public/toio_control.js
+++ p5js-toio/public/toio_control.js
@@ -1,6 +1,9 @@
 // This is the client-side javascript that gives commands to the actual toio hardware
 // Uses P5Toio
 
+// Spain
+// Canada - calgary, toronto
+// Denmark
 
 const CUBE_NAME_RED = "red";
 const CUBE_ID_RED = "toio Core Cube-D1haa";
@@ -11,18 +14,37 @@ const CUBE_ID_BLACK = "toio Core Cube-D1h";
 const CUBE_ID_BLUE = "toio Core Cube-H1A";
 const CUBE_NAME_BLUE = "blue";
 
+const CUBE_ID_GREEN = "toio Core Cube-H1Aaaa";
+const CUBE_NAME_GREEN = "green";
+
 const connectedCubeArray = [];
 const knownCubesById = {};
 const cubeNameToId = {};
 cubeNameToId[CUBE_NAME_RED] = 0;
 cubeNameToId[CUBE_NAME_BLACK] = 1;
 cubeNameToId[CUBE_NAME_BLUE] = 2;
+cubeNameToId[CUBE_NAME_GREEN] = 3;
 
 const MOTOR_MIN_SPEED = 8;
 const MOTOR_MAX_SPEED = 115;
 const MOTOR_TURN_SPEED = 13;
 const TURN_90DEG_DURATION = 58; //just comes from guess and check. use together with motor speed 12
 const TURN_DURATION_PER_DEGREE = TURN_90DEG_DURATION / 90; 
+
+// Times are actually given in 1-255 then multiplied by 10 to get milliseconds
+// according to https://toio.github.io/toio-spec/en/docs/ble_motor
+// The duration in which the motor is controlled is specified with values ranging from 0 to 255. 
+//A value of 0 means that there is no time limit and the motor continues to run at the specified
+// speed until the next write operation is performed. For values ranging between 1 and 255, 
+// the motor for 10 times the specified value in milliseconds, then stops.
+
+const DEFAULT_TIME = 100;
+const MAX_INSTR_TIME = 255;
+const POLY_SIDE_TIME = 160;
+const MAX_DISTANCE = 10; //? not sure what a good max is
+
+const CM_PER_SECOND = 2.05; // at motor speed 8
+//5.5cm at motor speed 8 for 1000 ms (send motor 100 as duration)
 
 // {prevActionDone: number}
 let cubeStates = [];
@@ -44,13 +66,14 @@ socket.on('bot_command', function (msg) {
 });
 
 function tryExecCommand(timestamp, cubeState, username, commandFn, duration) {
-  console.log(cubeState);
-  console.log(username);
-  if (duration = 0) {
+  console.log("tryExecCommand: " + username);
+  console.log("duration:" + duration);
+  duration = Math.round(duration);
+  if (duration == 0) {
     duration = 1; // hack!!! just trying to preven eternal commands from running accidentally
   }
-  if (duration > MAX_TIME) {
-    duration = MAX_TIME;
+  if (duration > MAX_INSTR_TIME) {
+    duration = MAX_INSTR_TIME;
   }
   if (timestamp > cubeState.prevActionDone) {
     console.log("Do command");
@@ -65,26 +88,32 @@ function tryExecCommand(timestamp, cubeState, username, commandFn, duration) {
   }
 }
 
-const DEFAULT_TIME = 1350;
-const MAX_TIME = 2550;
-
 function drawPolygon(c, cState, userName, startTs, sideSpeed, numSides, sideCount) {
+  console.log("drawPolygon:" + startTs + " numSides:" + numSides + " sideCount:" + sideCount);
   if (sideCount < numSides * 2) {
     let angle = (numSides - 2) * 180 / numSides;
+    console.log("angle: " + angle);
     if (sideCount % 2 === 0) {
+      console.log("drawPolygon: Go straight");
       // Go straight
-      cState.prevActionDone = tryExecCommand(c, startTs, cState, userName, () => c.move(sideSpeed, sideSpeed, 1350), 1350);
-      setTimeout(() => drawPolygon(c, cState, userName, startTs + 1400, sideSpeed, sideCount + 1), 1400);
-    }
-    else {
-      var durationTurnExtraTime = 50;
-      var duration = turnDurationForDegrees(angle - 180);
-      var turnSpeed = MOTOR_TURN_SPEED;
-      if (angle - 180 < 0) {
-        turnSpeed = -turnSpeed;
+      cState.prevActionDone = tryExecCommand(startTs, cState, userName, () => c.move(sideSpeed, sideSpeed, POLY_SIDE_TIME), POLY_SIDE_TIME);
+      setTimeout(() => {
+        console.log("drawPolygon: in first setTimeout");
+        drawPolygon(c, cState, userName, startTs + POLY_SIDE_TIME * 10, sideSpeed, numSides, sideCount + 1);
       }
-      cState.prevActionDone = tryExecCommand(c, startTs, cState, userName, () => c.rotate(turnSpeed, duration), duration);
-      setTimeout(() => drawPolygon(c, cState, userName, startTs + duration + durationTurnExtraTime, sideSpeed, sideCount + 1), duration + durationTurnExtraTime);
+      , POLY_SIDE_TIME * 10);
+    }
+    else {      
+      console.log("drawPolygon: Rotate");
+      var duration = turnDurationForDegrees(180 - angle);
+      var turnSpeed = MOTOR_TURN_SPEED;
+      
+      console.log("Turn for: " + duration + "ms");
+      cState.prevActionDone = tryExecCommand(startTs, cState, userName, () => c.move(turnSpeed, -turnSpeed, duration), duration);
+      setTimeout(() => {
+        drawPolygon(c, cState, userName, startTs + POLY_SIDE_TIME * 10, sideSpeed, numSides, sideCount + 1)
+      }, 
+      POLY_SIDE_TIME * 10);
     }
   }
 }
@@ -120,6 +149,16 @@ async function asyncHandleCubeCommand(msg) {
                 }
               // duration	number	0	Motor control duration in msec. 0-2550( 0: Eternally ).
                 cubeState.prevActionDone = tryExecCommand(ts, cubeState, args[args.length - 1], () => cube.move(speed, speed, DEFAULT_TIME), DEFAULT_TIME);             
+            } else if (command === 'dist') {
+              var distance = args.shift(); // assume in centimeters
+              // figure out some maximum
+              if (distance > MAX_DISTANCE) {
+                distance = MAX_DISTANCE;
+              }
+              var duration = (distance / CM_PER_SECOND) * 100;
+              duration = Math.round(duration);
+              console.log("dist: " + distance + " dur:" + duration);
+              cubeState.prevActionDone = tryExecCommand(ts, cubeState, args[args.length - 1], () => cube.move(MOTOR_MIN_SPEED, MOTOR_MIN_SPEED, duration), duration);
             } else if (command === 'rotate') {
               // !{bot} rotate {degrees}
               var degrees = args.shift();
@@ -131,13 +170,18 @@ async function asyncHandleCubeCommand(msg) {
               if (duration == 0) {
                 duration = 1; // this is a hacky fix to try and prevent eternal duration
               }
+              console.log("rotate: duration: " + duration);
               cubeState.prevActionDone = tryExecCommand(ts, cubeState, args[args.length - 1], () => cube.move(turnSpeed, -turnSpeed, duration), duration);
             } else if (command === 'spin') {
               console.log("spinning");
               var speed = parseSpeed(args.shift(), 8);
               cubeState.prevActionDone = tryExecCommand(ts, cubeState, args[args.length - 1], () => cube.move(-speed, speed, DEFAULT_TIME), DEFAULT_TIME);
             } else if (command === 'poly') {
-              drawPolygon(cube, cubeState, "testUserName", ts, 15, 3, 0);
+              var numSides = args.shift();
+              if (numSides >= 3 && numSides <= 8) {
+              //function drawPolygon(c, cState, userName, startTs, sideSpeed, numSides, sideCount)
+                drawPolygon(cube, cubeState, args[args.length - 1], ts, 8, numSides, 0);
+              }
             } else {
               console.log(`* Unknown command ${command}`);
             }
